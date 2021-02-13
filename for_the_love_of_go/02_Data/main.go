15e5dd118fc01b8bacc4242e69f7d461fe2fc8e3
Piero Mamberti <piero.mamberti@gmail.com>
2021-02-13 19:23:49 +0000

    Add Goal 1 - Chapter 1

---
+++ for_the_love_of_go/02_Data/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var title string
+	var copies int
+	var author string
+	title = "The Happiness Project"
+	copies = 99
+	author = "Stephen King"
+	fmt.Println(title)
+	fmt.Println(copies)
+	fmt.Println(author)
+}
