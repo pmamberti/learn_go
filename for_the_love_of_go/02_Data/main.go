a45b660170fee5f97a8b34852c780ca8ee267d81
Piero Mamberti <piero.mamberti@gmail.com>
2021-02-13 20:44:00 +0000

    Finish chapter 2

--- for_the_love_of_go/02_Data/main.go
+++ for_the_love_of_go/02_Data/main.go
@@ -2,22 +2,39 @@ package main
 
 import "fmt"
 
+// Book represents information about a book
+type Book struct {
+	Title           string
+	Author          string
+	Series          string
+	SeriesNumber    int
+	Copies          int
+	PriceCents      int
+	DiscountPercent int
+	FinalPrice      int
+	Edition         bool
+}
+
+// NetPrice calculates the price of the book after the discount
+func NetPrice(a Book) int {
+	netDiscount := a.PriceCents * a.DiscountPercent / 100
+	return a.PriceCents - netDiscount
+}
+
 func main() {
 
-	// Book represents information about a book
-	type Book struct {
-		Title        string
-		Author       string
-		Series       string
-		SeriesNumber int
+	b := Book{
+		Title:           "Fundamentals",
+		Author:          "John Arundel",
+		Series:          "For the Love of Go",
+		SeriesNumber:    1,
+		Copies:          7,
+		PriceCents:      499,
+		DiscountPercent: 10,
 	}
 
-	var b = Book{
-		Title:        "Fundamentals",
-		Author:       "John Arundel",
-		Series:       "For the Love of Go",
-		SeriesNumber: 1,
-	}
+	b.FinalPrice = NetPrice(b)
 
-	fmt.Println(b)
+	fmt.Printf("Author: %v\nSeries: %v\nTitle: %v\nNumber in Series: %v\nPrice: %v USD\nDiscount: %v%\nFinal Price: %v\n", b.Author, b.Series, b.Title, b.SeriesNumber, b.PriceCents/100, b.DiscountPercent, b.FinalPrice)
+	fmt.Println(b.Edition)
 }
